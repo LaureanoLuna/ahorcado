92d4035bc0369b035ba5ba168b422847980d0086
LaureanoLuna <laureano.luna@est.fi.uncoma.edu.ar>
2024-02-05 00:09:19 -0300

    correcciones

--- v2/front/src/Juego/Game.jsx
+++ v2/front/src/Juego/Game.jsx
@@ -1,51 +1,30 @@
-import React, { useState } from "react";
-import usePalabraRandom from "../assets/Hooks/usePalabraRandom";
+import { useState } from "react";
 import Letra from "../assets/Components/Letra";
+import useHandleLetra from "../assets/Hooks/useHandleJuego.mjs";
 
 export default function Game() {
-  const { palabraJuego, getPalabraRandom, palabraAdivinar } =
-    usePalabraRandom();
   const [inputLetra, setInputLetra] = useState("");
+  const { handleLetra, errorCount, palabraAdivinar, resetWord, palabraJuego } =
+    useHandleLetra(inputLetra);
 
-  function handleLetra(e) {
-    setInputLetra(e.target.value);
-    Array.from(palabraJuego).map((letra, i) => {
-      if (letra.toLowerCase() === e.target.value.toLowerCase()) {
-        palabraAdivinar.current[i] = letra;
-      }
-    });
+  function setLetra(event) {
+    let letraIngresada = event.target.value;
+    setInputLetra(letraIngresada);
+    handleLetra(letraIngresada);
     setTimeout(() => {
       setInputLetra("");
-    }, 1000);
+    }, 300);
   }
+
+  function handleChangePalabra() {
+    resetWord();
+  }
+
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-evenly",
-        alignItems: "stretch",
-      }}
-    >
-      <div
-        style={{
-          fontFamily: "ui-monospace",
-          fontSize: "xx-large",
-          textTransform: "uppercase",
-          letterSpacing: "2px",
-        }}
-      >
-        {palabraJuego}
-      </div>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: `repeat(${palabraAdivinar.current.length}, 1fr)`,
-          gap: "10px",
-          margin: "2em",
-        }}
-      >
+    <div className="game-content">
+      <div className="content-errors">{errorCount}</div>
+      <div className="representation-game">{palabraJuego}</div>
+      <div id="palabra-adivinar">
         {palabraAdivinar.current.map((letra, index) => (
           <Letra letra={letra} key={index} />
         ))}
@@ -57,21 +36,19 @@ export default function Game() {
             height: "50px",
             marginBottom: "20px",
             textAlign: "center",
-            backgroundColor: "black",
+            backgroundColor: "white",
             border: "none",
             borderRadius: "5px",
-            fontFamily: "ui-monospace",
             fontSize: "xx-large",
-            color: "white",
+            color: "black",
             textTransform: "uppercase",
           }}
-          disabled={inputLetra !== "" ? true : false}
           value={inputLetra}
           type="text"
-          onChange={handleLetra}
+          onChange={setLetra}
         />
       </div>
-      <button onClick={getPalabraRandom}>Change</button>
+      <button onClick={handleChangePalabra}>Change</button>
     </div>
   );
 }
