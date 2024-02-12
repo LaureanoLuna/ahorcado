de0d179ba6662e2e55204a6bf9b63e2421e20ed5
LaureanoLuna <laureano.luna@est.fi.uncoma.edu.ar>
2024-02-12 00:59:41 -0300

    Aplicamos estilos en el menu

--- v2/front/src/Juego/Index.jsx
+++ v2/front/src/Juego/Index.jsx
@@ -1,71 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Index() {
+  useEffect(() => {
+    const handleClick = (e) => {
+      let link = e.target;
+      if (link.getAttribute("data-link") === "game") {
+        console.log(link);
+      }
+    };
+
+    document.querySelectorAll(".button-link-index")?.forEach((element) => {
+      element.addEventListener("click", handleClick);
+    });
+
+    // Devolver una función de limpieza para eliminar el event listener
+    return () => {
+      document.querySelectorAll(".button-link-index")?.forEach((element) => {
+        element.removeEventListener("click", handleClick);
+      });
+    };
+  }, []);
   return (
-    <div
-      style={{
-        width: "auto",
-        margin: "auto",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        height: "300px",
-      }}
-    >
-      <Link
-        style={{
-          minWidth: "30%",
-          border: "solid",
-          padding: "5px 10px",
-          borderRadius: "5px",
-          color: "black",
-          boxShadow: "5px 5px 1px black",
-        }}
-        to={"game"}
-      >
-        Jugar
-      </Link>
-      <Link
-        style={{
-          minWidth: "30%",
-          border: "solid",
-          padding: "5px 10px",
-          borderRadius: "5px",
-          color: "black",
-          boxShadow: "5px 5px 1px black",
-        }}
-        to={"point"}
-      >
-        Puntos
-      </Link>
-      <Link
-        style={{
-          minWidth: "30%",
-          border: "solid",
-          padding: "5px 10px",
-          borderRadius: "5px",
-          color: "black",
-          boxShadow: "5px 5px 1px black",
-        }}
-        to={"instruccions"}
-      >
-        Instrucciones
-      </Link>
-      <Link
-        style={{
-          minWidth: "30%",
-          border: "solid",
-          padding: "5px 10px",
-          borderRadius: "5px",
-          color: "black",
-          boxShadow: "5px 5px 1px black",
-        }}
-        to={"about"}
-      >
-        ¿Quienes Somos?
-      </Link>
+    <div className="container">
+      <div className="container-index">
+        <Link data-link="game" className="button-link-index" to={"game"}>
+          Jugar
+        </Link>
+        <Link data-link="point" className="button-link-index" to={"point"}>
+          Puntos
+        </Link>
+        <Link
+          data-link="instruccions"
+          className="button-link-index"
+          to={"instruccions"}
+        >
+          Instrucciones
+        </Link>
+        <Link data-link="about" className="button-link-index" to={"about"}>
+          ¿Quienes Somos?
+        </Link>
+      </div>
     </div>
   );
 }
