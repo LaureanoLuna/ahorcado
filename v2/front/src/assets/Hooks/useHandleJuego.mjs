63bb784601c9f019676ee79ffb49de601198fb05
LaureanoLuna <laureano.luna@est.fi.uncoma.edu.ar>
2024-06-14 18:38:31 -0300

    Documentamos el codigo

--- v2/front/src/assets/Hooks/useHandleJuego.mjs
+++ v2/front/src/assets/Hooks/useHandleJuego.mjs
@@ -1,34 +1,52 @@
 import { useEffect, useState } from "react";
 import usePalabraRandom from "./usePalabraRandom";
 
+/**
+ * Hook personalizado para manejar la lógica de ingreso de letras y el estado del juego.
+ */
 function useHandleLetra() {
-  const { palabraJuego, palabraAdivinar, getPalabraRandom } =
-    usePalabraRandom();
+  // Obtener la palabra de juego, la palabra a adivinar y la función para obtener una nueva palabra aleatoria
+  const { palabraJuego, palabraAdivinar, getPalabraRandom } = usePalabraRandom();
+
+  // Estado para el conteo de errores
   const [errorCount, setErrorCount] = useState(0);
 
+  /**
+   * Maneja la letra ingresada por el usuario.
+   * @param {string} e - La letra ingresada por el usuario.
+   */
   const handleLetra = (e = null) => {
     if (e === null) return;
     const letraIngresada = e.toLowerCase();
+
+    // Verificar si la letra está en la palabra de juego
     if (palabraJuego.includes(letraIngresada)) {
+      // Actualizar la palabra a adivinar con las letras correctas
       Array.from(palabraJuego).map((letra, i) => {
         if (letra.toLowerCase() === letraIngresada) {
           palabraAdivinar.current[i] = letra;
         }
       });
     } else {
+      // Incrementar el contador de errores si la letra no está en la palabra
       setErrorCount((prevErrorCount) => prevErrorCount + 1);
     }
   };
 
+  /**
+   * Reinicia la palabra de juego obteniendo una nueva palabra aleatoria.
+   */
   const resetWord = () => {
     getPalabraRandom();
   };
 
+  // Efecto que se ejecuta cuando la palabra de juego cambia
   useEffect(() => {
     handleLetra();
     setErrorCount(0);
   }, [palabraJuego]);
 
+  // Retornar las funciones y estados necesarios para el manejo de letras y el estado del juego
   return { handleLetra, errorCount, palabraAdivinar, resetWord, palabraJuego };
 }
 
--- v2/front/src/assets/Hooks/usePalabraRandom.js
+++ v2/front/src/assets/Hooks/usePalabraRandom.js
@@ -2,28 +2,51 @@ import { useEffect, useRef, useState } from "react";
 import { fetchPalabraRandom } from "../Function/fetchPalabraRandom.mjs";
 import { useLocation } from "react-router-dom";
 
+/**
+ * Hook personalizado para manejar la lógica de obtener una palabra aleatoria y 
+ * preparar los estados necesarios para el juego de adivinanza de palabras.
+ */
 function usePalabraRandom() {
+  // Obtener la ruta actual usando react-router
   const { pathname } = useLocation();
+
+  // Estado para almacenar la palabra de juego
   const [palabraJuego, setPalabraJuego] = useState("");
+
+  // Referencia para almacenar la palabra a adivinar, inicializada como un array vacío
   const palabraAdivinar = useRef([]);
 
+  /**
+   * Actualiza el almacenamiento local con la palabra dada.
+   * @param {string} palabra - La palabra a almacenar.
+   */
   const updateLocalStorage = (palabra) => {
     localStorage.setItem("palabra", palabra);
   };
 
+  /**
+   * Inicializa la palabra a adivinar como un array de la misma longitud que la palabra de juego, 
+   * con todos los valores establecidos en null.
+   * @param {string} palabra - La palabra de juego.
+   */
   const initializePalabraAdivinar = (palabra) => {
     palabraAdivinar.current = Array(palabra.length).fill(null);
   };
 
+  /**
+   * Obtiene una palabra aleatoria del servidor y actualiza los estados correspondientes.
+   * Si la palabra obtenida es la misma que la almacenada en el local storage, intenta obtener otra palabra.
+   */
   const getPalabraRandom = async () => {
     try {
       const palabra = await fetchPalabraRandom();
 
       if (!palabra || localStorage.getItem("palabra") === palabra) {
-        getPalabraRandom(); // Retry if conditions not met
+        getPalabraRandom(); // Reintentar si las condiciones no se cumplen
         return;
       }
 
+      // Actualizar local storage, estado de la palabra de juego y palabra a adivinar
       updateLocalStorage(palabra);
       setPalabraJuego(palabra);
       initializePalabraAdivinar(palabra);
@@ -32,13 +55,20 @@ function usePalabraRandom() {
     }
   };
 
+  /**
+   * Efecto que se ejecuta cuando la ruta cambia.
+   * Si la ruta es "/game", se obtiene una nueva palabra aleatoria.
+   */
   useEffect(() => {
     if (pathname === "/game") {
       getPalabraRandom();
     }
   }, [pathname]);
 
+  // Retornar los estados y funciones necesarios para el manejo de la palabra aleatoria
   return { palabraJuego, palabraAdivinar, getPalabraRandom };
 }
 
 export default usePalabraRandom;
+
+
