388a1265040633a97c2e46483d51968dfe2cf0e9
LaureanoLuna <laureano.luna@est.fi.uncoma.edu.ar>
2024-08-18 03:37:38 -0300

    s

--- v2/front/src/App.jsx
+++ v2/front/src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import Index from "./Juego/Index";
-import Game from "./Juego/Game";
+import Index from "./Juego/Index.jsx";
+import Game from "./Juego/Game.jsx";
 import { Route, Routes } from "react-router-dom";
-import Puntajes from "./Juego/Puntajes";
+import Puntajes from "./Juego/Puntajes.jsx";
 
 
 function App() {
